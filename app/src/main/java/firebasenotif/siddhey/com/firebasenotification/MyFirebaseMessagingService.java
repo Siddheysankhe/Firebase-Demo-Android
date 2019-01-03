bd4e6a3ff2024e2479d9aca8200d3157743a437d
siddhey <siddhey.sankhe@kyepot.com>
2019-01-03 12:11:44 +0530

    added image in fcm notification

--- app/src/main/java/firebasenotif/siddhey/com/firebasenotification/MyFirebaseMessagingService.java
+++ app/src/main/java/firebasenotif/siddhey/com/firebasenotification/MyFirebaseMessagingService.java
@@ -1,9 +1,22 @@
 package firebasenotif.siddhey.com.firebasenotification;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by siddhey on 30/12/18.
@@ -11,6 +24,8 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService{
     private static final String TAG = "FCM Service";
+    Bitmap bitmap;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // TODO: Handle FCM messages here.
@@ -19,5 +34,58 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService{
         // message, here is where that should be initiated.
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        Log.d(TAG,"Image Url:" + remoteMessage.getData());
+
+        String message = remoteMessage.getData().get("body");
+
+        String TrueOrFlase = remoteMessage.getData().get("AnotherActivity");
+
+        String imageUri = remoteMessage.getData().get("image");
+        bitmap = getBitmapfromUrl(imageUri);
+
+        sendNotification(message, bitmap, TrueOrFlase);
+
+    }
+
+    public Bitmap getBitmapfromUrl(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+            return bitmap;
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+
+        }
+    }
+
+    private void sendNotification(String messageBody, Bitmap image, String TrueOrFalse) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("AnotherActivity", TrueOrFalse);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setLargeIcon(image)/*Notification icon image*/
+                .setSmallIcon(R.drawable.firebase_icon)
+                .setContentTitle(messageBody)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(image))/*Notification with Image*/
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
